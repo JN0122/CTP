2f573268e573791c28caeae7c61453b27424e030
nifiz <nifizek@gmail.com>
2024-04-07 23:48:24 +0200

    Refactorization, error handling and summaries

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,7 +8,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.IO;
 
 namespace CTP
 {
@@ -22,32 +21,14 @@ namespace CTP
             InitializeComponent();
         }
 
-        //Po kliknieciu przycisku file picker od MC zwraca nam sciezke do pliku
+        // Po kliknieciu dostajemy sciezke do wybranego pliku i jego zawartosc
+        // Ewentualnie zostanie 
         private void FilePickerButton_Click(object sender, RoutedEventArgs e)
         {
+            
+            string FilePath = FilePicker.GetFilePath();
 
-            var file_reader_inst = new FileReader();
-
-            var dialog = new Microsoft.Win32.OpenFileDialog();
-            dialog.FileName = "NULL_PATH"; // Default file name
-            dialog.DefaultExt = ".txt"; // Default file extension
-            dialog.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
-
-            // Show open file dialog box
-            bool? result = dialog.ShowDialog();
-
-            // Process open file dialog box results
-            if (result == true && dialog.FileName != "NULL_PATH")
-            {
-                // Open document
-                string filename = dialog.FileName;
-
-                FilePickerButton.Content = "Wybierz nowy plik";
-
-                FilePathDisplay.Content = filename;
-
-                FileContentDisplay.Content = file_reader_inst.GetFileContent(filename);
-            }
+            string FileContentRaw = FilePicker.GetFileContent(FilePath);
         }
     }
 }
