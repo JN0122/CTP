e66a25e4fba2193ee675a3570a46b2502f148d3f
nifiz <nifizek@gmail.com>
2024-05-05 13:31:31 +0200

    First

---
+++ core/Column.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTP.core
+{
+    internal class Column
+    {
+
+        private string Name;
+        private string Rodzaj;
+        private double Vmin, Vmax;
+        private double Mmmin, Mmax;
+
+        public Column(string Name, string Rodzaj)
+        {
+            this.Name = Name;
+            this.Rodzaj = Rodzaj;
+
+        }
+    }
+}
