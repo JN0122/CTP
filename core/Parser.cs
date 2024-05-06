4bd48b07a48337cff6c001a16ad87eebd5bb27b3
JN0122 <jakubniewelt@icloud.com>
2024-05-06 23:00:35 +0200

    refactor and fix: show error box, prevent null path, handle multiple charts

--- core/Parser.cs
+++ core/Parser.cs
@@ -6,10 +6,10 @@ namespace CTP.core
     static class Parser
     {
         private static DataTable? _datatable;
-        private static CultureInfo cultureInfo = new("pl-PL");
+        private readonly static CultureInfo cultureInfo = new("pl-PL");
 
         ///<summary>
-        /// Tworzy obiekt DataTable z wartościami typu double, automatycznie wykrywa czy plik posiada nagłówek.
+        /// Tworzy obiekt DataTable z wartościami typu float, automatycznie wykrywa czy plik posiada nagłówek.
         ///</summary>
         public static DataTable Parse(string fileContent)
         {
@@ -22,21 +22,28 @@ namespace CTP.core
             };
 
             List<string> columnHeaders = new(lines[0].Split(delimiter));
-            bool firtLineHeader = !Double.TryParse(columnHeaders[0], out _);
+            bool firtLineHeader = !float.TryParse(columnHeaders[0], out _);
 
             for (int i = 0; i < columnHeaders.Count; i++)
             {
-                if (firtLineHeader) _datatable.Columns.Add(columnHeaders[i], typeof(double));
-                else _datatable.Columns.Add($"Column{i}", typeof(double));
+                _datatable.Columns.Add(firtLineHeader ? columnHeaders[i] : $"Column{i}", typeof(float));
             }
 
-            for (int i = 0; i < lines.Count; i++)
+            try
             {
-                if ((i == 0 && firtLineHeader) || String.IsNullOrEmpty(lines[i])) continue;
-                DataRow datarow = _datatable.NewRow();
-                datarow.ItemArray = lines[i].Split(delimiter);
-                _datatable.Rows.Add(datarow);
+                for (int i = 0; i < lines.Count; i++)
+                {
+                    if ((i == 0 && firtLineHeader) || String.IsNullOrEmpty(lines[i])) continue;
+                    DataRow datarow = _datatable.NewRow();
+                    datarow.ItemArray = lines[i].Split(delimiter);
+                    _datatable.Rows.Add(datarow);
+                }
             }
+            catch (ArgumentException) 
+            {
+                throw new Exception("File is not correct format! Please check the values in columns.");
+            }
+
             return _datatable;
         }
     }
