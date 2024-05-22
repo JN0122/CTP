e3d9cf9697e08e4b0e1c32d3c4e58ab02654e336
JN0122 <jakubniewelt@icloud.com>
2024-05-22 16:48:10 +0200

    add: all sensor chart

---
+++ Charts/AllSensorsChart.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.ObjectModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Serialization;
+using CommunityToolkit.Mvvm.ComponentModel;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra.Factorization;
+using SkiaSharp;
+
+namespace CTP.Charts;
+
+public partial class AllSensorsChart: ObservableObject
+{
+    private readonly List<ObservableCollection<ObservablePoint>> _movementValues = new() { new(), new(), new(), new() };
+    private readonly List<ObservableCollection<ObservablePoint>> _velocityValues = new() { new(), new(), new(), new() };
+    private readonly List<ObservableCollection<ObservablePoint>> _accelerationValues = new() { new(), new(), new(), new() };
+
+    public AllSensorsChart()
+    {
+        for (int i = 0; i < _movementValues.Count; i++)
+        {
+            MovementSeries.Add(CreateSeries(_movementValues[i]));
+            VelocitySeries.Add(CreateSeries(_velocityValues[i]));
+            AccelerationSeries.Add(CreateSeries(_accelerationValues[i]));
+        }
+
+        XAxes = new[] { new Axis() };
+        YAxes = new[] { new Axis() };
+    }
+
+    static ISeries CreateSeries(ObservableCollection<ObservablePoint>  _values)
+    {
+        return new LineSeries<ObservablePoint>()
+        {
+            Values = _values,
+            GeometryStroke = null,
+            GeometryFill = null,
+            DataPadding = new(0, 1),
+        };
+    }
+
+    public List<ISeries> MovementSeries { get; set; } = new();
+    public List<ISeries> VelocitySeries { get; set; } = new();
+    public List<ISeries> AccelerationSeries { get; set; } = new();
+    public Axis[] XAxes { get; set; }
+    public Axis[] YAxes { get; set; }
+    public List<float> Labels { get; set; } = new();
+
+
+    public void SetSensorValues(int index, List<float> Values, string SensorName) {
+        SetValues(_movementValues[index], Values);
+        MovementSeries[index].Name = SensorName;
+    }
+
+    private void SetValues(ObservableCollection<ObservablePoint> _seriesValues, List<float> Values)
+    {
+        if(Labels.Count != Values.Count) throw new Exception("Labels does not match Values");
+
+        _seriesValues.Clear();
+        for (int i = 0; i < Values.Count; i++)
+        {
+            _seriesValues.Add(new ObservablePoint(Labels[i], Values[i]));
+        }
+    }
+
+    public void ShowSensor(string SensorName)
+    {
+        ChangeSensorVisibility(SensorName, true);
+    }
+
+    public void HideSensor(string SensorName)
+    {
+        ChangeSensorVisibility(SensorName, false);
+    }
+
+    private void ChangeSensorVisibility(string SensorName, bool Show)
+    {
+        for (int i = 0; i < _movementValues.Count; i++)
+        {
+            if (MovementSeries[i].Name != SensorName) continue;
+            MovementSeries[i].IsVisible = Show;
+            return;
+        }
+        throw new Exception("Sensor does not exists on chart!");
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,9 @@
 ﻿using CTP.core;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.VisualElements;
+
 using Microsoft.VisualBasic;
 using System.Collections.ObjectModel;
 using System.Data;
@@ -39,6 +44,7 @@ namespace CTP
             }
 
             RawChart.AllValues = data.GetValues(1);
+
             ColViewModelInstance.addColumns(data);
         }
 
@@ -47,11 +53,8 @@ namespace CTP
             myTabControl.SelectedIndex = 1;
 
             XChart.AllValues = data.GetValues(1);
-
             VelocityChart.AllValues = data.GetVelocityValues(1);
-
             AccelerationChart.AllValues = data.GetAccelerationValues(1);
-            //NumberOfColumns = data.ColumnsCount();
 
             /*Trace.WriteLine(String.Join(", ", _timeValues));*/
         }
@@ -61,24 +64,60 @@ namespace CTP
 
         }
 
+        private void AddSensorToList(string Content)
+        {
+            CheckBox sensor_checkbox = new()
+            {
+                Content = Content,
+                IsChecked = true
+            };
+            sensor_checkbox.Checked += CheckBox_Checked;
+            sensor_checkbox.Unchecked += CheckBox_Unchecked;
+            Sensor_List.Children.Add(sensor_checkbox);
+        }
+
         private void FilePDFButton_Click(object sender, RoutedEventArgs e)
         {
-
+            AllSensorsChart.ShowSensor("Sensor 1");
         }
         
         private void FinishSensorConfiguration_Click(object sender, RoutedEventArgs e)
         {
             myTabControl.SelectedIndex = 2;
+            AllSensorsChart.Labels = data.GetValues(0);
+
+            Sensor_List.Children.Clear();
+            for (int i = 1; i < data.Table.Columns.Count; i++)
+            {
+                AddSensorToList("Sensor " + i);
+                AllSensorsChart.SetSensorValues(i-1, data.GetValues(i), "Sensor " + i);
+            }
         }
 
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
+            string? Content = GetCheckboxContent(sender);
 
+            if (Content == null) return;
+
+            AllSensorsChart.ShowSensor(Content);
         }
 
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
+            string? Content = GetCheckboxContent(sender);
 
+            if (Content == null) return;
+
+            AllSensorsChart.HideSensor(Content);
+        }
+
+        private static string? GetCheckboxContent(object sender)
+        {
+            if (sender.GetType() != typeof(CheckBox)) return null;
+            CheckBox sensor_checkbox = (CheckBox)sender;
+
+            return (string)sensor_checkbox.Content;
         }
     }
 }
